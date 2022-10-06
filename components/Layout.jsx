3be475c44419704f7d125ceda84ec9405cfbfd32
Tomo-ja <tomonao.hj@gmail.com>
2022-10-06 10:25:56 -0700

    add theme

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,17 @@
 import { Component } from "react";
+import { ThemeProvider } from "styled-components";
 import Header from "./Header/index";
+
+import theme from "../styles/theme";
 
 class Layout extends Component {
 	render() {
 			const { children } = this.props
 			return (
-				<>
+				<ThemeProvider theme={theme}>
 					<Header />
 					{children}
-				</>
+				</ThemeProvider>
 			)
 	}
 }
---
+++ styles/theme.js
@@ -0,0 +1,31 @@
+const theme = {
+	colors: 
+		{
+			black: '#06191D',
+			white: '#ffffff',
+			lightBlue: '#80CFF1',
+			blue: '#63A69F',
+			lightRed: '#F2836B',
+			yellow: '#FFD34E',
+		},
+	fontSize : 
+		{
+			xSmall: '12px',
+			small: '14px',
+			default: '16px',
+			middle: '20px',
+			large: '24px',
+			xLarge: '32px',
+			xxLarge: '52px'
+		},
+	spacing: 
+		{
+			small: '8px',
+			default: '16px',
+			middle: '24px',
+			large: '36px'
+		},
+	
+}
+
+export default theme
